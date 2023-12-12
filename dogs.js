7eb4073f8723d4f23bdb0ab6da8ca2e9d1865333
Matheus Souza <msouza6192@gmail.com>
2023-12-12 17:06:52 -0500

    first commit

---
+++ dogs.js
@@ -0,0 +1,17 @@
+const url = 'https://dog.ceo/api/breeds/image/random';
+async function fetchDog() {
+    try {
+        let response = await fetch(url);
+        let data = await response.json();
+        displayDog(data.message);
+    } catch (error) {
+        console.error('There was an error!', error);
+    }
+}
+function displayDog(imageUrl) {
+    const img = document.createElement('img');
+    img.src = imageUrl;
+    img.alt = "Random dog photo";
+    img.width = 500;
+    document.getElementById('dog').appendChild(img);
+}
